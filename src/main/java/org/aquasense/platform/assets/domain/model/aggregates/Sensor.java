0a3cf05740644de16f8acfdf9c2a0eaca9f504fd
Rexest01 <richardcrack01@gmail.com>
2025-06-13 11:48:29 -0500

    feat(assets): sensor data entity

---
+++ src/main/java/org/aquasense/platform/assets/domain/model/aggregates/Sensor.java
@@ -0,0 +1,40 @@
+package org.aquasense.platform.assets.domain.model.aggregates;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.aquasense.platform.assets.domain.model.commands.CreateSensorCommand;
+import org.aquasense.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "sensors")
+public class Sensor  extends AuditableAbstractAggregateRoot<Sensor> {
+
+    private int oxygen_level;
+
+    private int pH_level;
+
+    private int water_temp_level;
+
+    private LocalDateTime last_update;
+
+    public Sensor() {}
+
+    public Sensor(int oxygen_level, int pH_level, int water_temp_level) {
+        this.oxygen_level = oxygen_level;
+        this.pH_level = pH_level;
+        this.water_temp_level = water_temp_level;
+    }
+
+    public Sensor(CreateSensorCommand command) {
+        this.oxygen_level = command.oxygen_level();
+        this.pH_level = command.pH_level();
+        this.water_temp_level = command.water_temp_level();
+    }
+}
