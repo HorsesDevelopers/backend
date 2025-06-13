b0b64ccd9c49f1ed788e6c3fece200233fbacef9
Rexest01 <richardcrack01@gmail.com>
2025-06-13 11:48:53 -0500

    feat(assets): device repository

---
+++ src/main/java/org/aquasense/platform/assets/infrastructure/persistence/jpa/repository/DeviceRepository.java
@@ -0,0 +1,10 @@
+package org.aquasense.platform.assets.infrastructure.persistence.jpa.repository;
+
+import org.aquasense.platform.assets.domain.model.aggregates.Sensor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/*
+@Repository
+public interface DeviceRepository extends JpaRepository<Device,Long> {
+}
+*/
