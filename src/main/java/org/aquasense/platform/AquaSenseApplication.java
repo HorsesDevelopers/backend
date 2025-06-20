df7c3df14587f1e374627e995dd13f5a3f82ed29
alumnos <kenerc15@gmail.com>
2025-06-20 13:12:13 -0500

    feat(feature/assets-and-resources): n

--- src/main/java/org/aquasense/platform/AquaSenseApplication.java
+++ src/main/java/org/aquasense/platform/AquaSenseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+//hi
 @EnableJpaAuditing
 @SpringBootApplication
 public class AquaSenseApplication {
