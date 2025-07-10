1f33ee28de933e738d58bda3acea467656625efa
Charlie <u201923994@upc.edu.pe>
2025-07-10 12:22:33 -0500

    chore(om): web socket was added to om

--- src/main/java/org/aquasense/platform/ar/interfaces/resources/CreateSensorResource.java
+++ src/main/java/org/aquasense/platform/ar/interfaces/resources/CreateSensorResource.java
@@ -1,9 +1,9 @@
 package org.aquasense.platform.ar.interfaces.resources;
 
-public record CreateSensorResource(Long id,
-                                   int oxygenLevel,
-                                   //int pHLevel,
-                                   String sensorType,
-                                   String status,
-                                   Float temperatureLevel) {
+public record CreateSensorResource(
+        Long pondId,
+        String type,
+        float maxValue,
+        float minValue
+) {
 }
--- src/main/java/org/aquasense/platform/ar/interfaces/transform/CreateSensorCommandFromResourceAssembler.java
+++ src/main/java/org/aquasense/platform/ar/interfaces/transform/CreateSensorCommandFromResourceAssembler.java
@@ -5,10 +5,11 @@ import org.aquasense.platform.ar.interfaces.resources.CreateSensorResource;
 
 public class CreateSensorCommandFromResourceAssembler {
     public static CreateSensorCommand toCommandFromResource(CreateSensorResource resource){
-        return new CreateSensorCommand(resource.oxygenLevel(),
-                resource.sensorType(),
-                resource.status(),
-                resource.temperatureLevel() != null ? resource.temperatureLevel() : 0.0f
+        return new CreateSensorCommand(
+                resource.pondId(),
+                resource.type(),
+                resource.maxValue(),
+                resource.minValue()
                 );
     }
 }
